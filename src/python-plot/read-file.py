c217c881aca316cbf98eaf3c0021d152f7d0c537
Goof <g.blokker@student.tudelft.nl>
2019-01-08 10:06:56 +0100

    Updated removed conflicts, python correct again

--- src/python-plot/read-file.py
+++ src/python-plot/read-file.py
@@ -7,6 +7,7 @@ colors = ['r', 'g', 'b', 'y']
 x = []
 ys = dict()
 ys2 = dict()
+ys3 = dict()
 
 with open('../../output/result.txt','r') as csvfile:
 	plots = csv.reader(csvfile, delimiter=',')
@@ -16,8 +17,10 @@ with open('../../output/result.txt','r') as csvfile:
 			if key not in ys:
 				ys[key] = []
 				ys2[key] = []
+				ys3[key] =[]
 			ys[int(row[0])].append(float(row[1]))
 			ys2[int(row[0])].append(float(row[2]))
+			ys3[int(row[0])].append(float(row[3]))
 		else:
 			index = row[0].index('t')
 			x.append(float(row[0][index + 4:]))
@@ -29,40 +32,35 @@ for y in ys:
 	plt.ylabel('Distance travelled')	
 
 plt.subplot(122)
-for y in ys2:
-	plt.plot(x, ys2[y], colors[y % len(colors)])
+for y in ys3:
+	plt.plot(x, ys3[y], colors[y % len(colors)])
 	plt.xlabel('Time')
 	plt.ylabel('Speed')
 plt.show()
     
 # First set up the figure, the axis, and the plot element we want to animate
 fig = plt.figure()
-<<<<<<< Updated upstream
 ax = plt.axes(xlim=(0, 2500), ylim=(-2, 2))
-y = np.zeros(10)
-=======
-ax = plt.axes(xlim=(0, 4000), ylim=(-2, 2))
->>>>>>> Stashed changes
 
-t = np.zeros((len(x),10))
-for n in range(0,10):
+t1 = np.zeros((len(x),len(ys)))
+t2 = np.zeros((len(x),len(ys)))
+for n in range(0,len(ys)):
     temp = ys[n]
+    temp2 = ys2[n]
     for i in range(0,len(x)):
-        t[i][n] = temp[i]
-print(t[0])
-point, = ax.plot(t[0],y,'o')
+        t1[i][n] = temp[i]
+        t2[i][n] = temp2[i]
+print(t1[0])
+print(t2[0])
+point, = ax.plot(t1[0],t2[0],'o')
 
 # animation function.  This is called sequentially
 def animate(i):
-    x = t[i]
-    y = np.zeros(10)
+    x = t1[i]
+    y = t2[i]
     point.set_data(x, y)
     return point
 
-<<<<<<< Updated upstream
-ani = animation.FuncAnimation(fig, animate, interval=0.05, blit = False, repeat = False)
-=======
-ani = animation.FuncAnimation(fig, animate, interval=5, blit = False, repeat = False)
->>>>>>> Stashed changes
+ani = animation.FuncAnimation(fig, animate, interval=0.5, blit = False, repeat = False)
 
 plt.show()
